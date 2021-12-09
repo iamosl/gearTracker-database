5744cbdf021885535ee871a8421995591158fe78
iamosl <osl.lavangad@gmail.com>
2021-12-09 13:59:07 +0530

    Made change to student table refernce to user table. Rest is as done by hemc

--- alter.sql
+++ alter.sql
@@ -1,7 +1,7 @@
 use geartracker_db;
 
 alter table user add constraint fk_user_stud FOREIGN KEY (student) references student(surrogate_id) ON DELETE CASCADE;
-alter table student add constraint fk_stud_user FOREIGN KEY (surrogate_id) references user(surrogate_id) ON DELETE CASCADE;
+alter table student add constraint fk_stud_user FOREIGN KEY (surrogate_id) references user(student) ON DELETE CASCADE;
 alter table requests add constraint fk_req_user FOREIGN KEY (id_user) references user(surrogate_id) ON DELETE CASCADE;
 alter table requests add constraint fk_req_equip FOREIGN KEY (id_equipment) references equipment(surrogate_id);
 alter table user_role add constraint fk_usro_user FOREIGN KEY (id_user) references user(surrogate_id) ON DELETE CASCADE;
